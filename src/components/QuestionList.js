8666dc0d8150498cbc43308b45a513ff93070ed0
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-12 11:50:29 +0300

    get, post and delete working

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,44 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
+import {useState, useEffect} from "react";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then((response)=> response.json())
+    .then((questions) =>{
+      console.log(questions)
+      setQuestions(questions)
+    })
+  }, [])
+
+  function handleDeleteClick(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => {
+        const newQuestions = questions.filter((question) => question.id !== id);
+        setQuestions(newQuestions);
+      });
+  }
+
+
+  const questionList = questions.map((question) => (
+    <QuestionItem
+    key={question.id}
+    question={question}
+    onDeleteClick={handleDeleteClick}
+     />
+  ))
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */}
+      {questionList}
+      </ul>
     </section>
   );
 }
